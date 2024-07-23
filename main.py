6d2c99dfa028d3596fc4a9d0905d1ac81ef11afe
pranaydo <pranaydodake7@gmail.com>
2024-07-23 19:16:35 +0530

    cors rigin fixed

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 
 import models, schemas
 from database import SessionLocal, engine
+from fastapi.middleware.cors import CORSMiddleware
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -11,6 +13,25 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+app = FastAPI()
+
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+# ]
+
+origins = ["http://localhost:3000"] 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency
 def get_db():
